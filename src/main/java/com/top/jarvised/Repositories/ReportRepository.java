a0f4fa60bcf3e052fd18ecc5d42198061da82b3c
austinalex1995 <aealex1995@gmail.com>
2025-11-05 15:20:08 -0500

    feat: added Comment and Report repositories

---
+++ src/main/java/com/top/jarvised/Repositories/ReportRepository.java
@@ -0,0 +1,9 @@
+package com.top.jarvised.Repositories;
+
+import com.top.jarvised.Entities.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
