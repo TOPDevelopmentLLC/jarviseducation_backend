30c6278e4d0e3a15f56fea9ae17076665a41711a
austinalex1995 <aealex1995@gmail.com>
2025-11-19 16:47:01 -0500

    feat: added delete admin endpoint

--- src/main/java/com/top/jarvised/Controllers/AdministratorController.java
+++ src/main/java/com/top/jarvised/Controllers/AdministratorController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,11 @@ public class AdministratorController {
     @GetMapping("/administrators")
     public List<Administrator> getAllAdministrators() {
         return administratorService.getAllAdministrators();
+    }
+
+    @DeleteMapping("/administrator/{id}")
+    public Map<String, String> deleteAdministrator(@PathVariable Long id) {
+        administratorService.deleteAdministrator(id);
+        return Map.of("message", "Administrator deleted successfully");
     }
 }
